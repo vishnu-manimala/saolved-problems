26f98b36cc73523bafb0cb5548bd2e801e08c2dd
vishnuManimala <vishnumanimala8133@gmail.com>
2024-08-06 15:40:33 +0530

    reverse string

---
+++ string.js
@@ -0,0 +1,18 @@
+//  reverse word inplace from string
+
+// function reverse(s){
+//     let strings = s.split(' '); //splits words from string and stores
+//     stringRev = []
+//     strings.forEach(word=>{
+//         let revWord = ""
+//         for(i = word.length-1; i>=0; i--){
+//             revWord += word [i]; //reverses each word in array
+//         }
+//         stringRev.push(revWord); // adds reversed word to array
+//     })
+//     return stringRev.join(' '); // joins reversed words to string and returns
+// }
+// console.log(reverse("hi aswin"));
+
+
+
